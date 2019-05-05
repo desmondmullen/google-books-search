19fc0e3e3a97f6bf12c5f4640f29e20341ab9116
Desmond Mullen <dsm@busylittlestudios.com>
2019-05-05 16:22:32 -0400

    working

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
-const path = require("path");
 const routes = require("./routes");
 const mongoose = require('mongoose');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-const booksController = require('./controllers/booksController')
 
-// Define middleware here
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  io.emit('chat message', 'test ' + Date.now());
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
@@ -15,18 +18,14 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// app.use('/api', booksController)
-
 app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch");
 
-// Send every other request to the React app
-// Define any API routes before this runs
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`🌎 ==> API server now on port ${PORT}`);
 });
+
+// server.listen(3000, function () {
+//   console.log(`🌎 ==> socket listening on port 3000`);
+// });
