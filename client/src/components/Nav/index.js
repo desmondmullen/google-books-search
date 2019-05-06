c23fb40dfa053c5bd46aaab69c46d757db1e9f41
Desmond Mullen <dsm@busylittlestudios.com>
2019-05-05 23:37:30 -0400

    final

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 function Nav () {
   return (
     <>
-      <div className='navbar'><span className='nav-title'>Google Books Search</span> <div className='navbar-links'><a className='nav-link' href='/search'>Search</a> | <a className='nav-link' href='/saved'>Saved</a></div></div>
+      <div className='navbar'><span className='nav-title'>Google Books Search</span> <div className='navbar-links'><NavLink
+        to="/search"
+        className='nav-link'
+        activeStyle={{
+          fontWeight: "bold",
+          textDecoration: 'none'
+        }}
+      >Search</NavLink> | <NavLink
+        to="/saved"
+        className='nav-link'
+        activeStyle={{
+          fontWeight: "bold",
+          textDecoration: 'none'
+        }}
+      >Saved</NavLink></div></div>
+      {/* <div className='navbar'><span className='nav-title'>Google Books Search</span> <div className='navbar-links'><a className='nav-link' href='/search'>Search</a> | <a className='nav-link' href='/saved'>Saved</a></div></div> */}
     </ >
   );
 }
